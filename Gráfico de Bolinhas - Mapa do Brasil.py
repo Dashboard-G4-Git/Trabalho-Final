b4d0672162d5081d1e441c529a47e62f9a4a8fd9
Estevao468 <101183695+Estevao468@users.noreply.github.com>
2022-04-04 19:12:02 -0300

    Create Gráfico de Bolinhas - Mapa do Brasil.py

---
+++ Gráfico de Bolinhas - Mapa do Brasil.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import plotly.express as px
+from folium import plugins
+import numpy as np
+import folium
+import json
+
+df = pd.read_csv(('caso.csv'), parse_dates=['date'])
+df.dropna()
+df.head(100)
+
+df2 = pd.read_csv('https://raw.githubusercontent.com/sandeco/CanalSandeco/master/covid-19/cidades_brasil.csv')
+indexCities = df2.set_index('codigo_ibge')
+indexCities.head(100)
+
+cities = df.loc[df.place_type == 'city', :]
+cities.place_type.unique()
+cities = cities.join(indexCities, on='city_ibge_code')
+
+geo_last = cities.loc[cities.is_last == True, ['date','city', 'latitude', 'longitude', 'state', 'confirmed', 'deaths']]
+geo_last = geo_last.dropna(subset=['longitude'])
+geo_last = geo_last.dropna(subset=['latitude'])
+geo_last.head(100)
+
+m = folium.Map(location=[-9, -50],
+                zoom_start=5)#tiles='cartodbpositron'
+m.add_child(folium.LatLngPopup())
+
+for i in range(0, len(geo_last)):
+    folium.Circle(
+        location=[geo_last.iloc[i]['latitude'], geo_last.iloc[i]['longitude']],
+        color='crimson', fill='crimson',
+        tooltip =   '<li><bold>Cidade :       '+str(geo_last.iloc[i]['city'])+
+                    '<li><bold>Confirmados :  '+str(geo_last.iloc[i]['confirmed'])+
+                    '<li><bold>Regional :     '+str(geo_last.iloc[i]['state'])+
+                    '<li><bold>Mortes :       '+str(geo_last.iloc[i]['deaths']),
+        radius=int(geo_last.iloc[i]['confirmed'])/3).add_to(m)
+m
